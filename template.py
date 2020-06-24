5aeb5cf0009ee1cc8b4abef4564e0ae82cbf64a0
WtrN <nishi.w.136@gmail.com>
2020-06-24 09:45:42 +0900

    6/23しょうじん

---
+++ template.py
@@ -0,0 +1,15 @@
+import bisect,collections,copy,heapq,itertools,math,numpy,string
+from fractions import gcd
+import sys
+def I(): return int(sys.stdin.readline().rstrip())
+def LI(): return list(map(int,sys.stdin.readline().rstrip().split())) # tupleも検討する。
+def S(): return sys.stdin.readline().rstrip()
+def LS(): return list(sys.stdin.readline().rstrip().split())
+
+
+def main():
+    N = I()
+    A = [LI() for _ in range(N)]
+
+if __name__ == '__main__':
+    main()
